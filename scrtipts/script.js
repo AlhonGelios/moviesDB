68c79a8303ef08627bd8bfcd5eb8b2a422340af1
AlhonGelios <Geliosal39hon@gmail.com>
2020-07-02 13:00:39 +0300

    lession2

--- scrtipts/script.js
+++ scrtipts/script.js
@@ -12,7 +12,18 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     let lastFilms = prompt("Один из последних просмотренных фильмов?",""),
     ratioFilms = prompt("На сколько оцените его?", "");
-    personalMovieDB.movies[lastFilms] = ratioFilms;
+    if (lastFilms == "" || lastFilms == null || ratioFilms == "" || ratioFilms == null) {
+        i--;
+        alert ("Вы ввели некоррекиные данные");
+    } else {personalMovieDB.movies[lastFilms] = ratioFilms;}
 }
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else alert("Произошла ошибка");
 
 console.log(personalMovieDB);
