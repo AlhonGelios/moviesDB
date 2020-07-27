7e9ee62b7f7974c990d65a9d7e09ae06ab7c67fb
Gelios <Geliosal39hon@gmail.com>
2020-07-27 21:02:23 +0300

    lession 6 отработка

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
+// Возьмите свой код из предыдущей практики
 'use strict';
 
 const movieDB = {
@@ -27,7 +31,10 @@ const movieDB = {
 const   adv = document.querySelectorAll(".promo__adv img"),
         bgPromo = document.querySelector(".promo__bg"),
         genre = bgPromo.querySelector(".promo__genre"),
-        interactiveItem = document.querySelectorAll(".promo__interactive-item");
+        moveList = document.querySelector(".promo__interactive-list"),
+        deletefilm = moveList.querySelectorAll(".delete"),
+        addForm = document.querySelector(".add"),
+        input = addForm.querySelector("input");
 
 
 adv.forEach(item =>{
@@ -36,7 +43,43 @@ adv.forEach(item =>{
 genre.textContent = "драма";
 bgPromo.style.background = "url(../img/bg.jpg) center center/cover no-repeat";
 
+movieDB.movies.forEach(event => {
+    event.toUpperCase();
+});
 movieDB.movies.sort();
-interactiveItem.forEach((item , i) => {
-    item.innerHTML = `${i + 1}. ${movieDB.movies[i]} <div class="delete"></div>`;
+moveList.innerHTML = "";
+
+movieDB.movies.forEach((item, i) => {
+    moveList.innerHTML +=   `<li class="promo__interactive-item">${i + 1}. ${item}
+                                <div class="delete"></div>
+                            </li>`;
+});
+
+
+addForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    if (input.value == "") {
+        return alert("Вы не ввели название фильма.");
+    }
+
+    movieDB.movies.push(input.value);
+    input.value = "";
+
+    movieDB.movies.forEach(event => {
+        event.toUpperCase();
+    });
+    movieDB.movies.sort();
+
+    moveList.innerHTML = "";
+
+    movieDB.movies.forEach((item, i) => {
+        if (item.length > 21) {
+            item = item.slice(0,22) + "...";
+        }
+        moveList.innerHTML +=   `<li class="promo__interactive-item">${i + 1}. ${item}
+                                <div class="delete"></div>
+                            </li>`;
+    });
+    
 });
